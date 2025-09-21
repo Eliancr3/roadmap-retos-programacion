7200a9e0bb236d0b531e66b5a81196fbee7e8207
Eliancr3 <cruzreyeselian@gmail.com>
2025-09-21 10:38:26 -0400

    Create Eliancr3.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Eliancr3.py
@@ -0,0 +1,24 @@
+#python.org
+# comentario en linea
+"""
+Comentario
+en varias 
+lineas
+"""
+'''
+Comentario
+en varias 
+lineas
+'''
+my_variable = "Vaiable"
+my_variable = "Otro valor de la Vaiable"
+
+MY_CONSTANTE = "CONSTANTE" # por Convencion
+
+int = 1
+float = 1.5
+bool = True
+bool = False
+string = "Cadena de texto"
+
+print("¡Hola, Python")
